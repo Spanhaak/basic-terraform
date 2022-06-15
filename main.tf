08a0ac989ef1059510d8d6d1253140f608654e9f
Marco Spanhaak <mspanhaak@gmail.com>
2022-06-15 17:45:55 +0200

    feat: added a bucket creation example

--- main.tf
+++ main.tf
@@ -1,6 +1,21 @@
+#
+# Use the Google Cloud Platform Provider
+#
 provider "google" {
   project       = var.project
   region        = var.region
   zone          = var.zone
 }
 
+
+
+#
+# Let's create a bucket using Terraform
+#
+resource "google_storage_bucket" "first-bucket" {
+  name          = "horby-bucket1"                   # Name of the bucket
+  location      = "EU"                              # Location of the bucket
+  force_destroy = true                              # If bucket is destroyed, destroy all containing objects also
+
+  uniform_bucket_level_access = true                # Set uniform bucket level access
+}
